2014cc9f3573ba70227345e50e6e5dd18908c2d9
Plan49 <anthonyplanisek1@gmail.com>
2021-06-30 14:53:58 -0400

    got it to display the homepage

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: 'pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
   }
