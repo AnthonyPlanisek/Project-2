a799254b0334ed46199556290473091feaffb692
Emmett Rushin <emmettrushin@gmail.com>
2021-07-02 18:14:07 -0400

    added to db.js

--- models/DB.js
+++ models/DB.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/config");
+const bcrypt = require("bcrypt");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -27,16 +32,30 @@ User.init(
       },
     },
     gameWins: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
       },
+    },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "db",
+    modelName: "user",
   }
 );
 
