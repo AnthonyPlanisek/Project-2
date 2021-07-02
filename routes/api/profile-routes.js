c8aa921ea9f75353b9a61ddb7cb40c09dd238366
Plan49 <anthonyplanisek1@gmail.com>
2021-07-01 20:39:32 -0400

    update branch

--- routes/api/profile-routes.js
+++ routes/api/profile-routes.js
@@ -3,6 +3,7 @@ const { requiresAuth } = require('express-openid-connect')
 const router = require('express').Router()
 
 router.get('/', requiresAuth(), (req, res) => {
+//   console.log('odic!!!!', req.oidc)
   res.send(JSON.stringify(req.oidc.user))
 })
 
