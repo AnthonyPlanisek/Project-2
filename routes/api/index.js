52924cdf420212bebf47a796e5e37708b03032f6
Emmett Rushin <emmettrushin@gmail.com>
2021-07-02 18:59:00 -0400

    fixed index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
-const router = require('express').Router()
-const homepageRoutes = require('./homepage-routes')
-const profileRoutes = require('./profile-routes')
-router.use('/homepage', homepageRoutes)
-router.use('/profile', profileRoutes)
-module.exports = router
+const router = require("express").Router();
+const homepageRoutes = require("./homepage-routes");
+const profileRoutes = require("./profile-routes");
+
+router.use("/homepage", homepageRoutes);
+
+router.use("/profile", profileRoutes);
+
+module.exports = router;
