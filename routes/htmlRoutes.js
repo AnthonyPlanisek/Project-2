d49fbdfd8192ab8e52bbc023fd095e2f1e43c77d
Plan49 <anthonyplanisek1@gmail.com>
2021-07-07 16:24:40 -0400

    changed user model and routes associated with images

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -28,6 +28,7 @@ module.exports = (db) => {
         }
         // console.log(user);
         res.render('profile', user)
+        console.log(req.session.passport.user.id)
       })
     } else {
       res.redirect('/')
