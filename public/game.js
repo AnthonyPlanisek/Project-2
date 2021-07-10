aef35efb85416577028bd10c7c93f95ae16dfa30
Plan49 <anthonyplanisek1@gmail.com>
2021-07-10 12:34:30 -0400

    change

---
+++ public/game.js
@@ -0,0 +1,16 @@
+let exampleName
+const xhr = new XMLHttpRequest()
+xhr.withCredentials = true
+
+xhr.addEventListener('readystatechange', function () {
+  if (this.readyState === 4) {
+    const parsedData = JSON.parse(this.responseText)
+    console.log('!!!!!', parsedData)
+    console.log(parsedData.userInfo)
+    console.log(parsedData.userInfo.userName)
+    exampleName = parsedData.userInfo.userName
+    document.getElementById('welcomeCardname').innerHTML = exampleName
+  }
+})
+xhr.open('GET', 'http://localhost:3333/authuser') // change to heroku
+xhr.send()
