76ab3b90a2d5f409e8356b679df04c1ec759218f
Emmett Rushin <emmettrushin@gmail.com>
2021-07-10 11:05:44 -0400

    removed unused code

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ if (app.get('env') !== 'test') {
   app.use(morgan('dev')) // Hook up the HTTP logger
 }
 
-// app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, 'public')))
 require('./config/passport')(db, app, passport) // pass passport for configuration
 
